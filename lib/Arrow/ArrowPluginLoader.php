5463147583f339608dc4d072e6e8ba03dd28ea05
Darshan Sawardekar <darshan@sawardekar.org>
2014-05-13 23:01:43 +0530

    Adds ifdef to prevent class redeclaration.

--- lib/Arrow/ArrowPluginLoader.php
+++ lib/Arrow/ArrowPluginLoader.php
@@ -1,84 +1,88 @@
 <?php
 
-class ArrowPluginLoader {
+if (class_exists('ArrowPluginLoader') === false) {
 
-  static public $instance = null;
-  static public function getInstance() {
-    if (is_null(self::$instance)) {
-      self::$instance = new ArrowPluginLoader();
+  class ArrowPluginLoader {
+
+    static public $instance = null;
+    static public function getInstance() {
+      if (is_null(self::$instance)) {
+        self::$instance = new ArrowPluginLoader();
+      }
+
+      return self::$instance;
     }
 
-    return self::$instance;
-  }
+    public $plugins = array();
 
-  public $plugins = array();
-
-  public function __construct() {
-    add_action('plugins_loaded', array($this, 'load'));
-  }
-
-  public function register($name, $arrowVersion, $callback) {
-    if ($this->isRegistered($name)) {
-      return;
+    public function __construct() {
+      add_action('plugins_loaded', array($this, 'load'));
     }
 
-    $plugin = array(
-      'name' => $name,
-      'arrowVersion' => $arrowVersion,
-      'callback' => $callback
-    );
+    public function register($name, $arrowVersion, $callback) {
+      if ($this->isRegistered($name)) {
+        return;
+      }
 
-    $this->plugins[$name] = $plugin;
-  }
+      $plugin = array(
+        'name' => $name,
+        'arrowVersion' => $arrowVersion,
+        'callback' => $callback
+      );
 
-  function isRegistered($name) {
-    return array_key_exists($name, $this->plugins);
-  }
-
-  function load() {
-    $sorted = $this->sortPlugins();
-
-    foreach ($sorted as $plugin) {
-      $this->loadPlugin($plugin);
-    }
-  }
-
-  function loadPlugin(&$plugin) {
-    $callback = $plugin['callback'];
-    $name     = $plugin['name'];
-
-    if (!is_null($callback)) {
-      call_user_func($callback, $name);
+      $this->plugins[$name] = $plugin;
     }
 
-    $this->sendPluginEvent($name, 'loaded');
-    $this->sendPluginEvent($name, 'ready');
-  }
-
-  function sendPluginEvent($name, $eventType) {
-    $action = 'arrow-plugin-' . $name . "-$eventType";
-    do_action($action, $name);
-  }
-
-  function sortPlugins() {
-    $plugins = array_values($this->plugins);
-    usort($plugins, array($this, 'comparePlugins'));
-
-    return $plugins;
-  }
-
-  /* order is flipped because we want descending order */
-  function comparePlugins(&$a, &$b) {
-    $versionA = $a['arrowVersion'];
-    $versionB = $b['arrowVersion'];
-
-    if (version_compare($versionA, $versionB, '<')) {
-      return 1;
-    } elseif (version_compare($versionA, $versionB, '>')) {
-      return -1;
-    } else {
-      return 0;
+    function isRegistered($name) {
+      return array_key_exists($name, $this->plugins);
     }
+
+    function load() {
+      $sorted = $this->sortPlugins();
+
+      foreach ($sorted as $plugin) {
+        $this->loadPlugin($plugin);
+      }
+    }
+
+    function loadPlugin(&$plugin) {
+      $callback = $plugin['callback'];
+      $name     = $plugin['name'];
+
+      if (!is_null($callback)) {
+        call_user_func($callback, $name);
+      }
+
+      $this->sendPluginEvent($name, 'loaded');
+      $this->sendPluginEvent($name, 'ready');
+    }
+
+    function sendPluginEvent($name, $eventType) {
+      $action = 'arrow-plugin-' . $name . "-$eventType";
+      do_action($action, $name);
+    }
+
+    function sortPlugins() {
+      $plugins = array_values($this->plugins);
+      usort($plugins, array($this, 'comparePlugins'));
+
+      return $plugins;
+    }
+
+    /* order is flipped because we want descending order */
+    function comparePlugins(&$a, &$b) {
+      $versionA = $a['arrowVersion'];
+      $versionB = $b['arrowVersion'];
+
+      if (version_compare($versionA, $versionB, '<')) {
+        return 1;
+      } elseif (version_compare($versionA, $versionB, '>')) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+
   }
 
 }
