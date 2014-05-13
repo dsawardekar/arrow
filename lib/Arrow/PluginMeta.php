b72aa5b0ef5e4fd0c3dd16517f0f755feb127b7c
Darshan Sawardekar <darshan@sawardekar.org>
2014-05-13 18:46:18 +0530

    Adds PluginMeta and tests.

---
+++ lib/Arrow/PluginMeta.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Arrow;
+
+class PluginMeta {
+
+  protected $version           = '0.0.0';
+  protected $file              = null;
+  protected $slug              = null;
+  protected $dir               = null;
+  protected $optionsKey        = null;
+  protected $optionsPageTitle  = null;
+  protected $displayName       = null;
+  protected $defaultOptions    = array();
+  protected $scriptOptions     = array('in_footer' => true);
+  protected $stylesheetOptions = array('media' => 'all');
+
+  function __construct($file) {
+    $this->file = $file;
+  }
+
+  function getVersion() {
+    return $this->version;
+  }
+
+  function getFile() {
+    return $this->file;
+  }
+
+  function getSlug() {
+    if (is_null($this->slug)) {
+      $this->slug = basename($this->getFile(), '.php');
+    }
+
+    return $this->slug;
+  }
+
+  function getDir() {
+    if (is_null($this->dir)) {
+      $this->dir = dirname($this->getFile());
+    }
+
+    return $this->dir;
+  }
+
+  function getOptionsKey() {
+    if (is_null($this->optionsKey)) {
+      $this->optionsKey = $this->getSlug() . '-options';
+    }
+
+    return $this->optionsKey;
+  }
+
+  function getOptionsCapability() {
+    return 'manage_options';
+  }
+
+  function getDisplayName() {
+    if (is_null($this->displayName)) {
+      $this->displayName  = str_replace('-', ' ', $this->getSlug());
+      $this->displayName  = str_replace('wp', 'WP', $this->displayName);
+      $this->displayName  = ucwords($this->displayName);
+    }
+
+    return $this->displayName;
+  }
+
+  function getOptionsPageTitle() {
+    if (is_null($this->optionsPageTitle)) {
+      $this->optionsPageTitle .= $this->getDisplayName() . ' | Settings';
+    }
+
+    return $this->optionsPageTitle;
+  }
+
+  function getOptionsMenuTitle() {
+    return $this->getDisplayName();
+  }
+
+  function getOptionsPageSlug() {
+    return $this->getSlug();
+  }
+
+  function getOptionsMenuSlug() {
+    return $this->getSlug();
+  }
+
+  function getDefaultOptions() {
+    return $this->defaultOptions;
+  }
+
+  function getOptionsUrl() {
+    return admin_url(
+      'options-general.php?page=' . $this->getOptionsMenuSlug()
+    );
+  }
+
+  function getScriptOptions() {
+    if (!array_key_exists('version', $this->scriptOptions)) {
+      $this->scriptOptions['version'] = $this->getVersion();
+    }
+
+    return $this->scriptOptions;
+  }
+
+  function getStylesheetOptions() {
+    if (!array_key_exists('version', $this->stylesheetOptions)) {
+      $this->stylesheetOptions['version'] = $this->getVersion();
+    }
+
+    return $this->stylesheetOptions;
+  }
+}
---
+++ test/Arrow/PluginMetaTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Arrow;
+
+class PluginMetaTest extends \WP_UnitTestCase {
+
+  public $meta;
+
+  function setUp() {
+    parent::setUp();
+
+    $this->meta = new PluginMeta(getcwd() . '/my-plugin.php');
+  }
+
+  function test_it_store_path_to_main_plugin_file() {
+    $actual = $this->meta->getFile();
+    $this->assertEquals(getcwd() . '/my-plugin.php', $actual);
+  }
+
+  function test_it_can_build_slug_from_file_name() {
+    $actual = $this->meta->getSlug();
+    $this->assertEquals('my-plugin', $actual);
+  }
+
+  function test_it_stores_directory_of_main_plugin_file() {
+    $this->meta = new PluginMeta('foo/my-plugin.php');
+    $actual = $this->meta->getDir();
+    $this->assertEquals('foo', $actual);
+  }
+
+  function test_it_can_build_default_options_key() {
+    $actual = $this->meta->getOptionsKey();
+    $this->assertEquals('my-plugin-options', $actual);
+  }
+
+  function test_it_has_default_options_capability() {
+    $actual = $this->meta->getOptionsCapability();
+    $this->assertEquals('manage_options', $actual);
+  }
+
+  function test_it_has_default_display_name() {
+    $actual = $this->meta->getDisplayName();
+    $this->assertEquals('My Plugin', $actual);
+  }
+
+  function test_it_has_default_options_page_title() {
+    $meta = new PluginMeta('wp-my-plugin/wp-my-plugin.php');
+    $actual = $meta->getOptionsPageTitle();
+    $this->assertEquals('WP My Plugin | Settings', $actual);
+  }
+
+  function test_it_has_options_menu_title() {
+    $actual = $this->meta->getOptionsMenuTitle();
+    $this->assertEquals('My Plugin', $actual);
+  }
+
+  function test_it_has_options_page_slug() {
+    $actual = $this->meta->getOptionsPageSlug();
+    $this->assertEquals('my-plugin', $actual);
+  }
+
+  function test_it_has_empty_default_options() {
+    $this->assertEquals(array(), $this->meta->getDefaultOptions());
+  }
+
+  function test_it_has_default_options_url() {
+    $actual = $this->meta->getOptionsUrl();
+    $expected = 'options-general.php?page=my-plugin';
+    $this->assertStringEndsWith($expected, $actual);
+  }
+
+  function test_it_has_a_default_version() {
+    $actual = $this->meta->getVersion();
+    $this->assertEquals('0.0.0', $actual);
+  }
+
+  function test_it_has_default_script_options() {
+    $options = $this->meta->getScriptOptions();
+    $this->assertEquals('0.0.0', $options['version']);
+    $this->assertTrue($options['in_footer']);
+  }
+
+  function test_it_has_default_stylesheet_options() {
+    $options = $this->meta->getStylesheetOptions();
+    $this->assertEquals('0.0.0', $options['version']);
+    $this->assertEquals('all', $options['media']);
+  }
+
+}
