ae4bb38bfcab0a0e434bc2fd12f82e6c82c063d5
Darshan Sawardekar <darshan@sawardekar.org>
2014-05-16 16:06:43 +0530

    Fixes bug with static API.

--- test/Arrow/ArrowPluginLoaderTest.php
+++ test/Arrow/ArrowPluginLoaderTest.php
@@ -170,6 +170,20 @@ class ArrowPluginLoaderTest extends \WP_UnitTestCase {
     $this->assertEquals($expected, $GLOBALS['arrowPlugins']);
   }
 
+  function test_it_can_load_plugins_via_static_api() {
+    $options = array('plugin' => 'TestArrowPlugin', 'arrowVersion' => '2.0.5');
+    ArrowPluginLoader::load($this->one, $options);
+    $options = array('plugin' => 'TestArrowPlugin', 'arrowVersion' => '2.7.1');
+    ArrowPluginLoader::load($this->two, $options);
+    $options = array('plugin' => 'TestArrowPlugin', 'arrowVersion' => '2.6.2');
+    ArrowPluginLoader::load($this->three, $options);
+
+    do_action('plugins_loaded');
+
+    $expected = array('one', 'three', 'two');
+    $this->assertEquals($expected, $GLOBALS['arrowPlugins']);
+  }
+
 }
 
 class TestArrowPlugin {
