e55a907113201e83548db80e649523368fae32a5
Darshan Sawardekar <darshan@sawardekar.org>
2014-06-20 12:13:02 +0530

    Moves ranking from collector to Ranker object.

--- lib/Arrow/Asset/Manifest/FileCollector.php
+++ lib/Arrow/Asset/Manifest/FileCollector.php
@@ -8,8 +8,7 @@ class FileCollector {
 
   function collect($dir, $toCollect) {
     if (is_array($toCollect)) {
-      $files = $this->sortFiles($dir, $toCollect);
-      $this->files = array_merge($this->files, $files);
+      $this->files = array_merge($this->files, $toCollect);
     } else {
       array_push($this->files, $toCollect);
     }
@@ -21,53 +20,6 @@ class FileCollector {
 
   function reset() {
     $this->files = array();
-  }
-
-  function hasLoader($dir) {
-    return file_exists($this->loaderPathFor($dir));
-  }
-
-  function loaderPathFor($dir) {
-    return "$dir/.loader";
-  }
-
-  function loadOrderFor($dir) {
-    if ($this->hasLoader($dir)) {
-      return file($this->loaderPathFor($dir), FILE_IGNORE_NEW_LINES);
-    } else {
-      return array();
-    }
-  }
-
-  function rankFiles($dir, $files) {
-    $loadOrder = $this->loadOrderFor($dir);
-    $ranks = array();
-    $total = count($files);
-
-    for ($i = 0; $i < $total; $i++) {
-      $file     = $files[$i];
-      $info     = pathinfo($file);
-      $filename = $info['filename'];
-      $result   = array_search($filename, $loadOrder);
-
-      /* if file is ahead the load order, it's rank is higher */
-      if ($result !== false) {
-        $rank = 100000 - $result;
-      } else {
-        $rank = 10000 - $i;
-      }
-
-      $ranks[$rank] = $file;
-    }
-
-    return $ranks;
-  }
-
-  function sortFiles($dir, $files) {
-    $ranks = $this->rankFiles($dir, $files);
-    krsort($ranks);
-
-    return array_values($ranks);
   }
 
 }
--- test/Arrow/Asset/Manifest/FileCollectorTest.php
+++ test/Arrow/Asset/Manifest/FileCollectorTest.php
@@ -14,11 +14,9 @@ class FileCollectorTest extends \PHPUnit_Framework_TestCase {
 
     $this->container = new Container();
     $this->container
-      ->singleton('manifestDirScanner', 'Arrow\Asset\Manifest\DirScanner')
       ->singleton('manifestFileCollector', 'Arrow\Asset\Manifest\FileCollector');
 
     $this->collector = $this->container->lookup('manifestFileCollector');
-    $this->scanner   = $this->container->lookup('manifestDirScanner');
   }
 
   function test_it_can_be_reset() {
@@ -26,84 +24,19 @@ class FileCollectorTest extends \PHPUnit_Framework_TestCase {
     $this->assertEmpty($this->collector->getFiles());
   }
 
-  function test_it_can_build_path_to_loader_file() {
-    $actual = $this->collector->loaderPathFor('foo');
-    $this->assertEquals('foo/.loader', $actual);
+  function test_it_can_store_files() {
+    $this->collector->files = array('foo');
+    $this->assertEquals(array('foo'), $this->collector->getFiles());
   }
 
-  function test_it_knows_if_loader_file_is_absent() {
-    $actual = $this->collector->hasLoader('foo');
-    $this->assertFalse($actual);
+  function test_it_can_collect_single_item() {
+    $this->collector->collect('foo', 'bar');
+    $this->assertEquals(array('bar'), $this->collector->getFiles());
   }
 
-  function test_it_knows_if_loader_file_is_present() {
-    $actual = $this->collector->hasLoader('test/manifest/inorder');
-    $this->assertTrue($actual);
-  }
-
-  function test_it_has_empty_load_order_if_loader_is_absent() {
-    $actual = $this->collector->loadOrderFor('test/manifest/models');
-    $this->assertEmpty($actual);
-  }
-
-  function test_it_has_correct_load_order_if_loader_is_present() {
-    $actual = $this->collector->loadOrderFor('test/manifest/inorder');
-    $expected = array('dolor', 'ipsum', 'lorem');
-    $this->assertEquals($expected, $actual);
-  }
-
-  /* integration tests */
-  function test_it_can_sort_files_without_load_order() {
-    $files  = glob('test/manifest/models/*.js');
-    $dir    = 'test/manifest/models';
-    $actual = $this->collector->sortFiles($dir, $files);
-    $expected = array(
-      'test/manifest/models/a.js',
-      'test/manifest/models/b.js'
-    );
-
-    $this->assertEquals($expected, $actual);
-  }
-
-  function test_it_can_rank_files_with_load_order() {
-    $files  = glob('test/manifest/inorder/*.js');
-    $dir    = 'test/manifest/inorder';
-    $actual = $this->collector->sortFiles($dir, $files);
-    $expected = array(
-      'test/manifest/inorder/dolor.js',
-      'test/manifest/inorder/ipsum.js',
-      'test/manifest/inorder/lorem.js'
-    );
-
-    $this->assertEquals($expected, $actual);
-  }
-
-  function test_it_can_collect_files_in_dir() {
-    $this->scanner->scan('test/manifest/models', 'js', true);
-    $actual = $this->collector->getFiles();
-    $expected = array(
-      'test/manifest/models/a.js',
-      'test/manifest/models/b.js',
-      'test/manifest/models/admin/a.js',
-      'test/manifest/models/admin/b.js',
-      'test/manifest/models/admin/c.js'
-    );
-
-    $this->assertEquals($expected, $actual);
-  }
-
-  function test_it_can_collect_files_in_dir_in_custom_order() {
-    $this->scanner->scan('test/manifest/inorder', 'js', true);
-    $actual = $this->collector->getFiles();
-    $expected = array(
-      'test/manifest/inorder/dolor.js',
-      'test/manifest/inorder/ipsum.js',
-      'test/manifest/inorder/lorem.js',
-      'test/manifest/inorder/public/a.js',
-      'test/manifest/inorder/public/b.js',
-    );
-
-    $this->assertEquals($expected, $actual);
+  function test_it_can_collect_multiple_items() {
+    $this->collector->collect('foo', array('one', 'two', 'three'));
+    $this->assertEquals(array('one', 'two', 'three'), $this->collector->getFiles());
   }
 
 }
